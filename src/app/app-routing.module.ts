f5cf3bc440449d465dde9b542237a9a62dea088d
ahmed <benamerahmed6@gmail.com>
2023-12-12 15:52:55 +0200

    v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ComptabiliteComponent } from './comptabilite/comptabilite.component';
+import { GcommercialComponent } from './gcommercial/gcommercial.component';
+import { GproductionComponent } from './gproduction/gproduction.component';
+import { HomeComponent } from './home/home.component';
+import { ManagmentComponent } from './managment/managment.component';
+import { ParamComponent } from './param/param.component';
+import { StockLogistiqueComponent } from './stock-logistique/stock-logistique.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent, pathMatch: 'full' , redirectTo: ''},
+  {path: 'stock_logistique', component: StockLogistiqueComponent},
+  {path: 'gcommercial', component: GcommercialComponent},
+  {path: 'gproduction', component: GproductionComponent},
+  {path: 'managment', component: ManagmentComponent},
+  {path: 'comptabilite', component: ComptabiliteComponent},
+  {path: 'param', component: ParamComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HomeComponent } from './home/home.component';
+import { ParamComponent } from './param/param.component';
+import { GcommercialComponent } from './gcommercial/gcommercial.component';
+import { GproductionComponent } from './gproduction/gproduction.component';
+import { ManagmentComponent } from './managment/managment.component';
+import { ComptabiliteComponent } from './comptabilite/comptabilite.component';
+import { BiReportingComponent } from './bi-reporting/bi-reporting.component';
+import { StockLogistiqueComponent } from './stock-logistique/stock-logistique.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HomeComponent,
+    ParamComponent,
+    GcommercialComponent,
+    GproductionComponent,
+    ManagmentComponent,
+    ComptabiliteComponent,
+    BiReportingComponent,
+    StockLogistiqueComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
